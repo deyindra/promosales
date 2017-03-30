a400464dff5f2f49329f326b6138f8f9fd8955e8
Rucha Kanade <r.kanade@criteo.com>
2017-03-30 11:09:54 -0700

    UI updates

---
+++ UI/E commerce template/js/elasticsearch-js/es.js
@@ -0,0 +1,35 @@
+var elasticsearch = require('./node_modules/elasticsearch/src/elasticsearch');
+
+var client = new elasticsearch.Client({
+  host: 'http://172.28.46.6:9200',
+  log: 'trace'
+});
+
+
+client.ping({
+  requestTimeout: 30000,
+}, function (error) {
+  if (error) {
+    console.error('elasticsearch cluster is down!');
+  } else {
+    console.log('All is well');
+  }
+});
+
+
+client.search({
+  index: 'test',
+  type: 'product',
+  body: {
+    "query": {
+            "ids" : {
+                "values" : ["1"]
+            }
+        }
+
+  }
+})
+
+function getPrice() {
+        return 10001;
+}
